35275aa4d02204750f959b02f7fa3757755a92e3
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-04 13:37:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logShout(string){
+  string = string.toUpperCase
+  console.log string
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
